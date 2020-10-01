eaec002e66322adc54d9bc3c0e2bbba1abb0c75a
Stéphane PATERNA <paterna@club-internet.fr>
2020-10-01 17:32:06 +0200

    Finalisation scrollbar

--- apiService.js
+++ apiService.js
@@ -1,45 +1,6 @@
-const APÎ_URL = "https://api.themoviedb.org/3/movie/";
-const API_KEY = "3a0449720017ece47d0711bc3f199995";
+const APÎ_URL = 'https://api.themoviedb.org/3/movie/';
+const API_KEY = '3a0449720017ece47d0711bc3f199995';
 
-//1
-// export function fetchMovie(movieId, cb) {
-//   const url = `${APÎ_URL}${movieId}?api_key=${API_KEY}`;
-
-//     var xhr_object = new XMLHttpRequest();
-//     xhr_object.open("GET", url, false);
-//     xhr_object.send(null);
-
-//     if (xhr_object.readyState == 4) {
-//       return cb(JSON.parse(xhr_object.responseText));
-//     }
-  
-// }
-
-//2
-// export default function fetchMovie(movieId) {
-//   const url = `${APÎ_URL}${movieId}?api_key=${API_KEY}`;
-
-//   return new Promise((resolve, reject) => {
-//     var xhr_object = new XMLHttpRequest();
-//     xhr_object.open("GET", url, false);
-//     xhr_object.send(null);
-
-//     if (xhr_object.readyState == 4) {
-//       resolve(JSON.parse(xhr_object.responseText));
-//     }
-//   });
-// }
-
-// export default function fetchMovie(movieId) {
-//   const url = `${APÎ_URL}${movieId}?api_key=${API_KEY}`;
-//   return fetch(url).then(res => {
-//     return res.json()
-//   }).then(movie => {
-//       return movie;
-//   })
-// }
-
-//3
 export default async function fetchMovie(movieId) {
   const url = `${APÎ_URL}${movieId}?api_key=${API_KEY}`;
   let res = await fetch(url);
--- index.js
+++ index.js
@@ -1,162 +1,133 @@
-import fetchMovie from "./apiService.js";
-import Header from "./components/Header.mjs";
-import { ModaleSeries } from "./components/Modales.mjs";
-import { ModaleMovies } from "./components/Modales.mjs";
-import { fetchNetflixOriginals } from "./apiService.js";
-import { fetchTrending } from "./apiService.js";
-import { fetchTopRated } from "./apiService.js";
-import { fetchByGenreMovies } from "./apiService.js";
-import { fetchNetflixDescription } from "./apiService.js";
-import { displayRechercheMovie } from "./input.js";
-
+import fetchMovie from './apiService.js';
+import Header from './components/Header.mjs';
+import { ModaleSeries } from './components/Modales.mjs';
+import { ModaleMovies } from './components/Modales.mjs';
+import { fetchNetflixOriginals } from './apiService.js';
+import { fetchTrending } from './apiService.js';
+import { fetchTopRated } from './apiService.js';
+import { fetchByGenreMovies } from './apiService.js';
+import { fetchNetflixDescription } from './apiService.js';
+import { displayRechercheMovie } from './input.js';
 
 // APPEL DE LA FONCTION displayRechercheMovie IMPORTEE DE input.js POUR LA RECHERCHE D'UN FILM
 // PAR SON NOM DANS LA BARRE DE RECHERCHE ///////////////////////////////////////////////////
 displayRechercheMovie();
 
-
-// DIFFERENTES METHODES POUR RECUPERER LES INFOS POUR LE HEADER /////////////////////////////
-
-// (() => {
-  //Callback
-//   const getResponse = (data) => {
-//     return data;
-//   };
-//   try {
-//     let movie = fetchMovie(157336, getResponse);
-//     console.log(movie);
-//     document.getElementById("header").innerHTML = Header(movie);
-//     document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
-//   }
-//   catch(e){
-//     console.log(e)
-//   }
-
-// })();
-
-//2
-// (() => {
-//     let movie = fetchMovie(157336).then(movie => {
-//         console.log(movie)
-//         document.getElementById("header").innerHTML = Header(movie);
-//         document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
-//     })
-
-// })();
-
-//3
+// RECCUPERATION DES INFOS POUR LE HEADER /////////////////////////////
 (async () => {
-    let movie = await fetchMovie(181812);
-    document.getElementById("header").innerHTML = Header(movie);
-    document.getElementById("header").style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
+  let movie = await fetchMovie(181812);
+  document.getElementById('header').innerHTML = Header(movie);
+  document.getElementById(
+    'header'
+  ).style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movie.backdrop_path})`;
 })();
 
 // FACTORISATION DISPLAY MOVIES ////////////////////////////////////////////////////////////////////////////////
-const displayMovie = async(movies,i,container,isNetflix = false) => {
+const displayMovie = async (movies, i, container, isNetflix = false) => {
   let movie = document.createElement('div');
   let image = document.createElement('img');
-  if(isNetflix){
-    movie.className = "movies__container--movie__netflix";
-    image.className = "movies__container--movie-image";
+  if (isNetflix) {
+    movie.className = 'movies__container--movie__netflix';
+    image.className = 'movies__container--movie-image';
     image.src = `https://image.tmdb.org/t/p/original/${movies[i].poster_path}`;
-    if(movies[i].poster_path === null){
+    if (movies[i].poster_path === null) {
       image.src = './img/Logo_Netflix.jpg';
     }
-  }else{
-    movie.className = "movies__container--movie";
-    image.className = "movies__container--movie-image";
+  } else {
+    movie.className = 'movies__container--movie';
+    image.className = 'movies__container--movie-image';
     image.src = `https://image.tmdb.org/t/p/original/${movies[i].backdrop_path}`;
-    if(movies[i].backdrop_path === null){
+    if (movies[i].backdrop_path === null) {
       image.src = './img/Logo_Netflix.jpg';
     }
   }
   let id = movies[i].id;
   container.appendChild(movie);
   movie.appendChild(image);
-    
-  image.addEventListener('click', async(event) => {
-    event.preventDefault();   
+
+  image.addEventListener('click', async (event) => {
+    event.preventDefault();
     let modale = document.querySelector('.modale_container');
     modale.style.display = 'block';
-    if(isNetflix){
+    if (isNetflix) {
       let serieName = await fetchNetflixDescription(id);
-      modale.innerHTML = ModaleSeries(serieName);    
-      modale.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${serieName.backdrop_path})`;  
-    }else{
-      let movieName = await fetchMovie(id); 
+      modale.innerHTML = ModaleSeries(serieName);
+      modale.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${serieName.backdrop_path})`;
+    } else {
+      let movieName = await fetchMovie(id);
       modale.innerHTML = ModaleMovies(movieName);
-      modale.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movieName.backdrop_path})`;      
+      modale.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${movieName.backdrop_path})`;
     }
     modale.style.backgroundSize = 'cover';
     let button = modale.querySelector('.modaleButton');
     button.addEventListener('click', (event) => {
       event.preventDefault();
       let modaleDiv = modale.querySelector('#modale');
-      modale.removeChild(modaleDiv)
-      modale.style.backgroundImage = "";
-      modale.style.display = "none";
+      modale.removeChild(modaleDiv);
+      modale.style.backgroundImage = '';
+      modale.style.display = 'none';
     });
   });
-}
+};
 
 // SERIES NETFLIX ORIGINALS ////////////////////////////////////////////////////////////////
-(async() => {
+(async () => {
   let netflixOriginals = await fetchNetflixOriginals();
-  let container = document.getElementById("movies-container-1");
+  let container = document.getElementById('movies-container-1');
   let movies = netflixOriginals.results;
-  for(let i = 0 ; i < movies.length ; i++){
-    displayMovie(movies,i,container,true); 
-  }    
+  for (let i = 0; i < movies.length; i++) {
+    displayMovie(movies, i, container, true);
+  }
 })();
 
 // FILMS TENDANCE ////////////////////////////////////////////////////////////////////////
-(async() => {
+(async () => {
   let netflixTrending = await fetchTrending();
-  let container = document.getElementById("movies-container-2");
+  let container = document.getElementById('movies-container-2');
   let movies = netflixTrending.results;
-  for(let i = 0 ; i < movies.length ; i++){
-    displayMovie(movies,i,container); 
-  }    
+  for (let i = 0; i < movies.length; i++) {
+    displayMovie(movies, i, container);
+  }
 })();
 
 // FILMS LES MIEUX NOTES //////////////////////////////////////////////////////////////
-(async() => {
+(async () => {
   let netflixTopRated = await fetchTopRated();
-  let container = document.getElementById("movies-container-3");
+  let container = document.getElementById('movies-container-3');
   let movies = netflixTopRated.results;
   for (let i = 0; i < movies.length; i++) {
-    displayMovie(movies,i,container); 
+    displayMovie(movies, i, container);
   }
 })();
 
 // FILMS PAR GENRE ////////////////////////////////////////////////////////////////////
 
 // Films par genre : ACTION //////////////////////////////////////////////////////////
-(async() => {
+(async () => {
   let netflixByGenreAction = await fetchByGenreMovies(28);
-  let container = document.getElementById("movies-container-4");
+  let container = document.getElementById('movies-container-4');
   let movies = netflixByGenreAction.results;
   for (let i = 0; i < movies.length; i++) {
-    displayMovie(movies,i,container); 
+    displayMovie(movies, i, container);
   }
 })();
 
 // Films par genre : COMEDY /////////////////////////////////////////////////////////////
-(async() => {
+(async () => {
   let netflixByGenreComedy = await fetchByGenreMovies(35);
-  let container = document.getElementById("movies-container-5");
+  let container = document.getElementById('movies-container-5');
   let movies = netflixByGenreComedy.results;
   for (let i = 0; i < movies.length; i++) {
-    displayMovie(movies,i,container); 
+    displayMovie(movies, i, container);
   }
 })();
 
 // Films par genre : DOCUMENTARY ///////////////////////////////////////////////////////
-(async() => {
+(async () => {
   let netflixByGenreDocumentary = await fetchByGenreMovies(99);
-  let container = document.getElementById("movies-container-6");
+  let container = document.getElementById('movies-container-6');
   let movies = netflixByGenreDocumentary.results;
   for (let i = 0; i < movies.length; i++) {
-    displayMovie(movies,i,container); 
+    displayMovie(movies, i, container);
   }
 })();
